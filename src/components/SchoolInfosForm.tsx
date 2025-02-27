ba0fbe4530bbf8e607180f93d1faff7c9f48bf56
Yousra-Yurine <bouhrizdaidjyousra@gmail.com>
2025-02-27 21:57:13 +0100

    authentification

--- src/components/SchoolInfosForm.tsx
+++ src/components/SchoolInfosForm.tsx
@@ -2,7 +2,6 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 import { FiPhone } from "react-icons/fi";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaSchool } from "react-icons/fa";
-import InputForm from "./ui/InputForm";
 
 interface SchoolProps {
   setDisplayed: React.Dispatch<React.SetStateAction<boolean>>;
@@ -26,14 +25,21 @@ interface SchoolProps {
 
 const SchoolInfoForm: React.FC<SchoolProps> = ({ setDisplayed, setSchoolInfos, schoolInfos }) => {
   const labels = [
-    { text: `Nom d'établissement`, icon: FaSchool, name: "Nom_détablissement", value: schoolInfos.Nom_détablissement },
-    { text: `Type d'établissement`, icon: FaSchool, name: "Type_détablissement", value: schoolInfos.Type_détablissement },
-    { text: "Adresse", icon: FaMapMarkerAlt, name: "Adress", value: schoolInfos.Adress },
-    { text: "Adresse Email", icon: MdOutlineEmail, name: "Adresse_Email", value: schoolInfos.Adresse_Email },
-    { text: "Numéro de Téléphone", icon: FiPhone, name: "Numéro_de_Téléphone", value: schoolInfos.Numéro_de_Téléphone },
+    { text: `Nom d'établissement`, icon: FaSchool, name: "Nom_détablissement", value: schoolInfos.Nom_détablissement , type:"text"  , selectElement:false},
+    { text: `Type d'établissement`, icon: FaSchool, name: "Type_détablissement", value: schoolInfos.Type_détablissement  , type:"text" ,  selectElement:true},
+    { text: "Adresse", icon: FaMapMarkerAlt, name: "Adress", value: schoolInfos.Adress , type:"text" , selectElement:false },
+    { text: "Adresse Email", icon: MdOutlineEmail, name: "Adresse_Email", value: schoolInfos.Adresse_Email , type:"email" , selectElement:false },
+    { text: "Numéro de Téléphone", icon: FiPhone, name: "Numéro_de_Téléphone", value: schoolInfos.Numéro_de_Téléphone, type:"text" , selectElement:false },
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSchoolInfos((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSchoolInfos((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
@@ -44,14 +50,33 @@ const SchoolInfoForm: React.FC<SchoolProps> = ({ setDisplayed, setSchoolInfos, s
     <form className="flex flex-col gap-4 items-center">
       <div className="w-full">
         {labels.map((label, index) => (
-          <InputForm
-            key={index}
-            Inputtext={label.text}
-            InputIcon={label.icon}
-            name={label.name} 
-            value={label.value} 
-            onChange={handleChange} 
-          />
+         <div className="flex flex-col items-start gap-1" key={index}>
+         <label>{label.text}</label>
+         <div className="relative w-full">
+             <label.icon className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+             {label.selectElement ? (
+             <select onChange={handleSelect} className="w-full rounded-[5px] h-[45px] border-gray-400 border-[2px] focus:border-[var(--color-secondary)] focus:outline-none pl-10">
+              <option value="Primaire">Primaire</option>
+              <option value="Moyenne">Moyenne</option>
+              <option value="Secondaire">Secondaire</option>
+
+             </select>
+             ) : (
+              <input
+             id={label.name}
+             name={label.name}
+             type={label.type}
+             value={label.value}
+             onChange={handleChange}
+             placeholder="Enter value..."
+             required
+             className="w-full rounded-[5px] h-[45px] border-gray-400 border-[2px] focus:border-[var(--color-secondary)] focus:outline-none pl-10"
+           />
+             )
+             }
+          
+         </div>
+       </div>
         ))}
       </div>
 
